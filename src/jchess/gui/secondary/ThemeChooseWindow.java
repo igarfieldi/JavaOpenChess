5406f5e0ec4cb2b83b42176f4d6aecd45637c54d
Aleksandar Stojnic <stojnicprime@hotmail.de>
2016-11-12 18:19:46 +0100

    Refactored ThemeChooseWindow.java

--- src/jchess/gui/secondary/ThemeChooseWindow.java
+++ src/jchess/gui/secondary/ThemeChooseWindow.java
@@ -21,6 +21,7 @@
 package jchess.gui.secondary;
 
 import java.awt.*;
+
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -32,155 +33,186 @@ import jchess.gui.GUI;
 
 import javax.swing.event.ListSelectionEvent;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import java.security.*;
+import java.net.*;
+import java.util.zip.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ThemeChooseWindow extends JDialog implements ActionListener, ListSelectionListener
 {
+	private static Logger log = Logger.getLogger(ThemeChooseWindow.class.getName());
+	
+	private JList<String> themesList;
+	private ImageIcon themePreviewImage;
+	private JButton themePreviewButton;
+	private JButton okButton;
+	
+	public ThemeChooseWindow(Frame parent) throws Exception
+	{
+		super(parent);
+		
+		ArrayList<String> themeNames = new ArrayList<String>();
+		getThemeNames(themeNames);
+		
+		if(themeNames.size() > 0)
+			createGUI(themeNames);
+		else
+			throw new Exception(Localization.getMessage("error_when_creating_theme_config_window"));
+	}
 
-    JList<String> themesList;
-    ImageIcon themePreview;
-    GridBagLayout gbl;
-    GridBagConstraints gbc;
-    JButton themePreviewButton;
-    JButton okButton;
+	private void getThemeNames(ArrayList<String> themeNames) throws IOException, Exception
+	{
+		CodeSource codeSource = JChessApp.class.getProtectionDomain().getCodeSource();
+		if(codeSource != null)
+		{
+			File jarPath = new File(codeSource.getLocation().toURI().getPath());
+			if(jarPath.isDirectory())
+				getThemesFromDirectory(themeNames, jarPath);
+			else
+				getThemesFromJAR(themeNames, codeSource);
+		}
+		else
+			throw new Exception(Localization.getMessage("error_when_creating_theme_config_window"));
+	}
 
-    public ThemeChooseWindow(Frame parent) throws Exception
-    {
-        super(parent);
-        
-        ArrayList<String> themeNames = new ArrayList<String>();
-        
-        java.security.CodeSource src = JChessApp.class.getProtectionDomain().getCodeSource();
-    	if(src != null)
-    	{
-    		File jarPath = new File(src.getLocation().getPath());
-    		if(jarPath.isDirectory())
-    		{
-    			// We're not in a JAR, but instead in some directory
-    			// => Scan through the directories to find the themes
-    			File themePath = new File(jarPath.getAbsolutePath() + File.separator + "jchess" +
-    									File.separator + "resources" + File.separator + "theme");
-    			File[] themeFiles = themePath.listFiles();
-    			for(int i = 0; i < themeFiles.length; i++)
-    			{
-    				if(themeFiles[i].isDirectory())
-    				{
-    					themeNames.add(themeFiles[i].getName());
-    				}
-    			}
-    		}
-    		else
-    		{
-    			// We're in a JAR
-    			// General pattern to extract a theme's name
-        		Pattern themePattern = Pattern.compile("/[^/]+/$");
-        		
-    			java.net.URL jar = src.getLocation();
-    			java.util.zip.ZipInputStream zip = new java.util.zip.ZipInputStream(jar.openStream());
-    			java.util.zip.ZipEntry entry;
-    			
-    			while((entry = zip.getNextEntry()) != null)
-    			{
-    				// Find the resources we're looking for (the themes)
-    				// TODO: If the beginning of the path is not specified we get lots of double matches...
-    				if(entry.getName().matches("jchess/resources/theme/[^/]+/$"))
-    				{
-    					Matcher themeMatcher = themePattern.matcher(entry.getName());
-    					if(themeMatcher.find())
-    					{
-    						// Remove the '/' from the path
-    						themeNames.add(themeMatcher.group(0).replace("/", ""));
-    					}
-    				}
-    			}
-    		}
-    	}
-    	else
-    	{
-            throw new Exception(Localization.getMessage("error_when_creating_theme_config_window"));
-    	}
-    	
-        if(themeNames.size() > 0)
-        {
-            this.setTitle(Localization.getMessage("choose_theme_window_title"));
-            Dimension winDim = new Dimension(550, 230);
-            this.setMinimumSize(winDim);
-            this.setMaximumSize(winDim);
-            this.setSize(winDim);
-            this.setResizable(false);
-            this.setLayout(null);
-            this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-            
-            // JList needs an array, so we convert it to one
-            this.themesList = new JList<String>(themeNames.toArray(new String[themeNames.size()]));
-            this.themesList.setLocation(new Point(10, 10));
-            this.themesList.setSize(new Dimension(100, 120));
-            this.add(this.themesList);
-            this.themesList.setSelectionMode(0);
-            this.themesList.addListSelectionListener(this);
-            
-            this.gbl = new GridBagLayout();
-            this.gbc = new GridBagConstraints();
-            try
-            {
-                this.themePreview = new ImageIcon(GUI.loadThemeImage("Preview.png"));
-            }
-            catch (java.lang.NullPointerException exc)
-            {
-                System.err.println("Cannot find preview image: " + exc);
-                this.themePreview = new ImageIcon(JChessApp.class.getResource("/jchess/resources/theme/noPreview.png"));
-                return ;
-            }
-            this.themePreviewButton = new JButton(this.themePreview);
-            this.themePreviewButton.setLocation(new Point(110, 10));
-            this.themePreviewButton.setSize(new Dimension(420, 120));
-            this.add(this.themePreviewButton);
-            this.okButton = new JButton("OK");
-            this.okButton.setLocation(new Point(175, 140));
-            this.okButton.setSize(new Dimension(200, 50));
-            this.add(this.okButton);
-            this.okButton.addActionListener(this);
-            this.setModal(true);
-        }
-        else
-        {
-            throw new Exception(Localization.getMessage("error_when_creating_theme_config_window"));
-        }
-    }
+	private void getThemesFromDirectory(ArrayList<String> themeNames, File jarPath)
+	{
+		File themePath = new File(jarPath.getAbsolutePath() + File.separator + "jchess" + File.separator
+		        + "resources" + File.separator + "theme");
+		File[] themeFiles = themePath.listFiles();
+		for(int i = 0; i < themeFiles.length; i++)
+		{
+			if(themeFiles[i].isDirectory())
+				themeNames.add(themeFiles[i].getName());
+		}
+	}
 
-    @Override
-    public void valueChanged(ListSelectionEvent event)
-    {
-        String themeName = this.themesList.getModel().getElementAt(this.themesList.getSelectedIndex()).toString();
-        this.themePreview = new ImageIcon(GUI.loadThemeImage("Preview.png", themeName));
-        this.themePreviewButton.setIcon(this.themePreview);
-    }
+	private void getThemesFromJAR(ArrayList<String> themeNames, CodeSource codeSource) throws IOException
+	{
+		// General pattern to extract a theme's name
+		Pattern themePattern = Pattern.compile("/[^/]+/$");
+		
+		URL jar = codeSource.getLocation();
+		ZipInputStream zip = new ZipInputStream(jar.openStream());
+		ZipEntry entry;
+		
+		while((entry = zip.getNextEntry()) != null)
+		{
+			// Find the resources we're looking for (the themes)
+			// TODO: If the beginning of the path is not specified we
+			// get lots of double matches...
+			if(entry.getName().matches("jchess/resources/theme/[^/]+/$"))
+			{
+				Matcher themeMatcher = themePattern.matcher(entry.getName());
+				if(themeMatcher.find())
+				{
+					// Remove the '/' from the path
+					themeNames.add(themeMatcher.group(0).replace("/", ""));
+				}
+			}
+		}
+	}
 
-    /** Method which is changing a pawn into queen, rook, bishop or knight
-     * @param arg0 Capt information about performed action
-     */
-    public void actionPerformed(ActionEvent evt)
-    {
-        if (evt.getSource() == this.okButton)
-        {
-            Properties prp = GUI.getConfigFile();
-            int element = this.themesList.getSelectedIndex();
-            String name = this.themesList.getModel().getElementAt(element).toString();
-            if (GUI.themeIsValid(name))
-            {
-                prp.setProperty("THEME", name);
-                if(!GUI.storeConfigFile(prp))
-                {
-                	System.err.println("Failed to save config with new theme!");
-                }
-                JOptionPane.showMessageDialog(this, Localization.getMessage("changes_visible_after_restart"));
-                this.setVisible(false);
+	private void createGUI(ArrayList<String> themeNames)
+	{
+		// JList needs an array, so we convert it to one
+		this.themesList = new JList<String>(themeNames.toArray(new String[themeNames.size()]));
+		this.themePreviewButton = new JButton(this.themePreviewImage);
+		this.okButton = new JButton("OK");
+		
+		setWindowProperties();
+		setThemeListProperties();
+		
+		testGettingPreviewImage();
+		
+		setButtonProperties(themePreviewButton, 110, 10, 420, 120, false);
+		setButtonProperties(okButton, 175, 140, 200, 50, true);
+	}
 
-            }
-            System.out.print(prp.getProperty("THEME"));
-        }
-    }
+	private void setWindowProperties()
+	{
+		this.setTitle(Localization.getMessage("choose_theme_window_title"));
+		this.setModalityType(ModalityType.APPLICATION_MODAL);
+		Dimension winDim = new Dimension(550, 230);
+		this.setMinimumSize(winDim);
+		this.setMaximumSize(winDim);
+		this.setSize(winDim);
+		this.setResizable(false);
+		this.setLayout(null);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+
+	private void setThemeListProperties()
+	{
+		this.themesList.setLocation(new Point(10, 10));
+		this.themesList.setSize(new Dimension(100, 120));
+		this.add(this.themesList);
+		this.themesList.setSelectionMode(0);
+		this.themesList.addListSelectionListener(this);
+	}
+
+	private void testGettingPreviewImage()
+	{
+		try
+		{
+			this.themePreviewImage = new ImageIcon(GUI.loadThemeImage("Preview.png"));
+		}
+		catch(NullPointerException exception)
+		{
+			log.log(Level.SEVERE, "Cannot find preview image: ", exception);
+			this.themePreviewImage = new ImageIcon(
+			        JChessApp.class.getResource("/jchess/resources/theme/noPreview.png"));
+		}
+	}
+
+	private void setButtonProperties(JButton button, int positionX, int positionY, int sizeX, int sizeY, Boolean isInteractable)
+	{
+		button.setLocation(new Point(positionX, positionY));
+		button.setSize(new Dimension(sizeX, sizeY));
+		this.add(button);
+		
+		if(isInteractable)
+			button.addActionListener(this);
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent event)
+	{
+		String themeName = this.themesList.getModel().getElementAt(this.themesList.getSelectedIndex()).toString();
+		this.themePreviewImage = new ImageIcon(GUI.loadThemeImage("Preview.png", themeName));
+		this.themePreviewButton.setIcon(this.themePreviewImage);
+	}
+
+	public void actionPerformed(ActionEvent event)
+	{
+		if(event.getSource() == this.okButton)
+			setTheme();
+	}
+
+	private void setTheme()
+	{
+		Properties properties = GUI.getConfigFile();
+		int selectedThemeIndex = this.themesList.getSelectedIndex();
+		String themeName = this.themesList.getModel().getElementAt(selectedThemeIndex).toString();
+		
+		if(GUI.themeIsValid(themeName))
+		{
+			properties.setProperty("THEME", themeName);
+			if(!GUI.storeConfigFile(properties))
+				log.log(Level.SEVERE, "Failed to save config with new theme!");
+			
+			JOptionPane.showMessageDialog(this, Localization.getMessage("changes_visible_after_restart"));
+			this.setVisible(false);
+			
+		}
+		log.info(properties.getProperty("THEME"));
+	}
 }
