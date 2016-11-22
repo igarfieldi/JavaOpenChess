bcb17f26012181ff2d85aaa440899373f6c4d74d
Aleksandar Stojnic <stojnicprime@hotmail.de>
2016-11-23 00:39:10 +0100

    - Fixed some issues after merging

--- src/main/jchess/gamelogic/field/ChessboardController.java
+++ src/main/jchess/gamelogic/field/ChessboardController.java
@@ -331,7 +331,7 @@ public class ChessboardController
 						color = "B";
 					}
 					
-					String newPiece = JChessApp.jcv.showPawnPromotionBox(color); // return
+					String newPiece = JChessApp.view.showPawnPromotionBox(color); // return
 					                                                             // name
 					                                                             // of
 					                                                             // new
